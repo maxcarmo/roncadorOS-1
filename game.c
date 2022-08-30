fea264918b9636be362c8261430347f114d8529b
shamior <shamior_@hotmail.com>
2022-08-30 14:55:01 -0300

    jogo rodando

--- game.c
+++ game.c
@@ -3,26 +3,60 @@
 #include "game.h"
 #include "keyboard.h"
 #include "memlayout.h"
+#include "rng.h"
+#include "input-event-codes.h"
+
+
+
+
+
+//defines the time wich the piece updates going down
+#define UPDATE_TIME 1000
+
+//defines the frame update time
+#define FRAME_TIME 50
+
+//in wich loop we update the piece
+#define UPDATE_LOOP (UPDATE_TIME/FRAME_TIME)
+
+piece get_next_piece();
+
+typedef enum states {RUNNING, PAUSED, OVER, MENU} STATE;
+
+struct GAME{
+    uint8 has_switched;
+    STATE state; 
+    uint8 loop_counter;
+    uint8 *field;
+    piece current_piece, next_piece[NEXT_MAX], shadow_piece, hold_piece;
+    uint32 next_idx;
+    uint32 score;
+    uint64 dt;
+}game;
+
+
+
+
 
 
 //IZSOTLJ
 extern uint8 tetrominoes[NUM_OF_TETROMINOS][BLOCKS_PER_TETROMINO] = {
     {
-        0,0,1,0,
-        0,0,1,0,
-        0,0,1,0,
-        0,0,1,0
-    },
-    {
-        0,0,1,0,
-        0,1,1,0,
-        0,1,0,0,
+        0,0,0,0,
+        1,1,1,1,
+        0,0,0,0,
         0,0,0,0
     },
     {
-        0,1,0,0,
+        1,1,0,0,
         0,1,1,0,
-        0,0,1,0,
+        0,0,0,0,
+        0,0,0,0
+    },
+    {
+        0,1,1,0,
+        1,1,0,0,
+        0,0,0,0,
         0,0,0,0
     },
     {
@@ -32,22 +66,22 @@ extern uint8 tetrominoes[NUM_OF_TETROMINOS][BLOCKS_PER_TETROMINO] = {
         0,0,0,0
     },
     {
-        0,0,1,0,
-        0,1,1,0,
-        0,0,1,0,
+        0,1,0,0,
+        1,1,1,0,
+        0,0,0,0,
         0,0,0,0
     },
     {
-        0,0,0,0,
-        0,1,1,0,
         0,0,1,0,
-        0,0,1,0
+        1,1,1,0,
+        0,0,0,0,
+        0,0,0,0
     },
     {
+        1,0,0,0,
+        1,1,1,0,
         0,0,0,0,
-        0,1,1,0,
-        0,1,0,0,
-        0,1,0,0
+        0,0,0,0
     }
 
 };
@@ -60,26 +94,47 @@ extern uint8 tetrominoes[NUM_OF_TETROMINOS][BLOCKS_PER_TETROMINO] = {
 *@param rotation: Rotação do tetromino 0,1,2,...,2^32 -1
 *@returns Posição do bloco rotacionado
 */
-uint32 rotate(uint32 x, uint32 y, uint32 rotation){
+uint32 rotate(uint32 x, uint32 y, uint32 rotation, piece_shape shape){
     /*      0 graus         90 graus        180 graus       270 graus
     *       00 01 02 03     12 08 04 00     15 14 13 12     03 07 11 15
     *       04 05 06 07     13 09 05 01     11 10 09 08     02 06 10 14
     *       08 09 10 11     14 10 06 02     07 06 05 04     01 05 09 13
     *       12 13 14 15     15 11 07 03     03 02 01 00     00 04 08 12
-    */ 
-    switch (rotation % 4){ //existem 4 rotações disponiveis
-        case 0: return TETROMINO_SIZE*y + x;            //  0 graus
-        case 1: return 12 + y - (x * TETROMINO_SIZE);   // 90 graus
-        case 2: return 15 - (y * TETROMINO_SIZE) - x;   //180 graus
-        case 3: return 3 - y + (x*TETROMINO_SIZE);      //270 graus
+    */
+   rotation = rotation % 4; //existem 4 rotações disponiveis
+    switch (shape){
+        case Z:
+        case S:
+        case L:
+        case J:
+        case T:
+        //essas pecas so usam um grid 3x3, assim o calculo é diferente para rotacionar
+            if (x == 3 || y == 3) return 3; //coluna 3 ou linha 3 eh vazio sempre, entao retorna uma posicao que esta vazia
+            switch (rotation){
+                case 0: return TETROMINO_SIZE*y + x;            //  0 graus
+                case 1: return 8 + y - (x * TETROMINO_SIZE);   // 90 graus
+                case 2: return 10 - (y * TETROMINO_SIZE) - x;   //180 graus
+                case 3: return 2 - y + (x*TETROMINO_SIZE);      //270 graus
+            }
+        case I:
+        case O:
+        //ja essas usam 4x4
+            switch (rotation){ 
+                case 0: return TETROMINO_SIZE*y + x;            //  0 graus
+                case 1: return 12 + y - (x * TETROMINO_SIZE);   // 90 graus
+                case 2: return 15 - (y * TETROMINO_SIZE) - x;   //180 graus
+                case 3: return 3 - y + (x*TETROMINO_SIZE);      //270 graus
+            }
+            
     }
+
     return 0;
 }
 
 
-void init_field(){
+void reset_field(){
     for (uint32 i = 0; i < FIELD_HEIGHT*FIELD_WIDTH; i++){
-        game_field[i] = EMPTY;
+        game.field[i] = EMPTY;
     }
 }
 
@@ -99,21 +154,22 @@ uint8 piece_fit(piece p, int walk_x, int walk_y, int rot){
             cx = p.x + x + walk_x;
             cy = p.y + y + walk_y;
 
-            block_index = rotate(x,y, p.rotation + rot);
+            block_index = rotate(x,y, p.rotation + rot, p.shape);
             field_index = cy * FIELD_WIDTH + cx;
 
-            if (tetrominoes[p.name][block_index]){ //se ha um bloco de tetris
+            if (tetrominoes[p.shape][block_index]){ //se ha um bloco de tetris
                 // e este bloco está 
                 if (cx < 0 || cx >= FIELD_WIDTH) return 0; // fora dos limites no x
-                if (cy < 0 || cy >= FIELD_HEIGHT) return 0; // fora dos limites no y
-                if (game_field[field_index] != EMPTY) return 0; //ou está em cima de outro bloco
+                if (cy < -4 || cy >= FIELD_HEIGHT) return 0; // fora dos limites no y
+                if (cy >= 0 && game.field[field_index] != EMPTY) return 0; //ou está em cima de outro bloco
                 // então retorno falso
 
             }
 
             if (
-                tetrominoes[p.name][block_index] && //se ha um bloco de tetris
-                game_field[field_index] != EMPTY    //em cima de outro bloco
+                cy >= 0 &&
+                tetrominoes[p.shape][block_index] && //se ha um bloco de tetris
+                game.field[field_index] != EMPTY    //em cima de outro bloco
             ) return 0; //entao retorno falso, pois a peça não se encaixa
             
         }
@@ -121,9 +177,49 @@ uint8 piece_fit(piece p, int walk_x, int walk_y, int rot){
     return 1;
 }
 
-void rotate_piece(piece *p){
+int rotate_piece(piece *p){
     if (piece_fit(*p, 0,0,1)){
         (p->rotation)++;
+        return 1;
+    }
+    return 0;
+}
+
+int move_piece_down(piece *p){
+    if (piece_fit(*p, 0, 1, 0)){
+        (p->y)++;
+        return 1;
+    }
+    return 0;
+}
+
+int move_piece_left_or_right(piece *p, int direction){
+    if (piece_fit(*p, direction, 0, 0)){
+        p->x += direction;
+        return 1;
+    }
+    return 0;
+}
+
+int hard_drop(piece *p){
+    int dropped = 0;
+    while(move_piece_down(p)) dropped++;
+    return dropped;
+}
+
+void mark_piece_in_field(piece p){
+    uint32 piece_offset = p.y * FIELD_WIDTH + p.x;
+    uint32 piece_index;
+    uint32 field_offset, field_index;
+    for (uint8 x = 0; x < TETROMINO_SIZE; x++){
+        for (uint8 y = 0; y < TETROMINO_SIZE; y++){
+            piece_index = rotate(x, y, p.rotation, p.shape);
+            if (tetrominoes[p.shape][piece_index]){ //se há um bloco nessa rotacao
+                field_offset = y * FIELD_WIDTH  + x;
+                field_index = piece_offset + field_offset;
+                game.field[field_index] = p.shape; //marco no field
+            }
+        }
     }
 }
 
@@ -135,60 +231,288 @@ void wait(uint32 ms){
     }
 }
 
-#define REAL_LOOP 750
+void switch_hold(){
+    if (game.has_switched) return;
+    piece temp = game.current_piece;
+    if (game.hold_piece.shape == EMPTY){
+        game.current_piece = get_next_piece();
+    }else{
+        game.current_piece = game.hold_piece;
+    }
+    game.hold_piece = temp;
+    game.hold_piece.x = PIECE_START;
+    game.hold_piece.y = 0;
+    game.hold_piece.rotation = 0;
+    if (game.hold_piece.shape != O){
+        game.hold_piece.y++;
+    }
+    game.has_switched = true;
+}
 
-#define LOOP_TIME_MS 50
-
-#define UPDATE_LOOP (REAL_LOOP/LOOP_TIME_MS)
-
-void game_loop(){
-    uint8 game_over = 0;
-    piece p, p2, p3;
-
-    p.name = Z;
-    p.x = 3;
-    p.y = 4;
+piece new_piece(){
+    piece p;
+    p.shape = get_random_number(0, NUM_OF_TETROMINOS);
+    p.x = PIECE_START;
+    p.y = 0;
     p.rotation = 0;
+    if (p.shape != O){
+        p.y++;
+    }
+    return p;
+}
 
 
-    uint64 t0, t1, t2;
-    t0 = 0;
-    int i = 0;
-    while (!game_over){
-        //frame calculation ======================
-        t1 = (*(uint64*) CLINT_MTIME) / 10000; // 25
-        //wait(400);
-        if (t1 - t0 < LOOP_TIME_MS){
-            wait(LOOP_TIME_MS-(t1-t0));
-            //printf("t1-t0: %h\n", t1-t0);
-        }else{
-            printf("fodeu\n");
+piece get_next_piece(){
+    piece p = game.next_piece[game.next_idx];
+    game.next_piece[game.next_idx] = new_piece();
+    game.next_idx = (game.next_idx + 1) % NEXT_MAX;
+    return p;
+}
+
+void reset_game(){
+    //reset all pieces
+    game.current_piece = new_piece();
+    for (int i = 0; i < NEXT_MAX; i++){
+        game.next_piece[i] = new_piece();
+    }
+    game.hold_piece.shape = EMPTY;
+
+    //reset score
+    game.score = 0;
+
+    //reset some other shit
+    game.has_switched = false;
+    game.loop_counter = 0;
+
+    //change state for running
+    game.state = RUNNING;
+
+    //reset field
+    reset_field();
+}
+
+void treat_keys(){
+    virtio_input_event event;
+    while(!av_events_queue.empty){
+        event = av_events_queue.get_event(&av_events_queue);
+        switch (game.state){
+            case MENU:
+                if (event.code == KEY_ENTER){
+                    game.state = RUNNING;
+                }
+                break;
+            case OVER:
+                if (event.code == KEY_ENTER || event.code == KEY_R){
+                    reset_game();
+                }
+                break;
+            case PAUSED:
+                if (event.code == KEY_P){
+                    game.state = RUNNING;
+                }
+                break;
+            case RUNNING: 
+                switch (event.code){
+                    case KEY_SPACE:
+                        hard_drop(&game.current_piece);
+                        game.loop_counter += UPDATE_LOOP;
+                        break;
+                    case KEY_LEFT:
+                        move_piece_left_or_right(&game.current_piece, -1);
+                        break;
+                    case KEY_RIGHT:
+                        move_piece_left_or_right(&game.current_piece, 1);
+                        break;
+                    case KEY_DOWN:
+                        move_piece_down(&game.current_piece);
+                        break;
+                    case KEY_UP:
+                        rotate_piece(&game.current_piece);
+                        break;
+                    case KEY_P:
+                        game.state = PAUSED;
+                        break;
+                    case KEY_C:
+                        switch_hold();
+                        break;
+                    case KEY_R:
+                        reset_game();
+                        break;
+                }
+                break;
         }
-        t0 = (*(uint64*) CLINT_MTIME) / 10000; //15
-        //update
-        if (!(i%UPDATE_LOOP)){
-            rotate_piece(&p);
-            if (!(i%(10*UPDATE_LOOP))){
-                p.name = (p.name + 1) % 7;
-                //keyboard_int();
+    }
+}
+
+void update_game_over_status(){
+    //SE HÁ ALGUM BLOCO NO START_OFFSET
+    uint32 range = START_OFFSET*FIELD_WIDTH;
+    for (int field_index = 0; field_index < range; field_index++){
+        if (game.field[field_index] != EMPTY){
+            game.state = OVER;
+            return;
+        }
+    }
+}
+
+int check_line(int piece_height){
+    int marked_offset, unmarked_offset;
+    int lines_marked = 0;
+    int marked;
+    int lines_unmarked = 0;
+    int field_index;
+    int line_start_check = TETROMINO_SIZE - 1;
+    if (piece_height + TETROMINO_SIZE > FIELD_HEIGHT){
+        line_start_check = FIELD_HEIGHT - piece_height - 1;
+    }
+    int piece_bottom = (piece_height + line_start_check) * FIELD_WIDTH;
+    for (int line = line_start_check; line >= 0; line--){
+        field_index = (piece_height + line) * FIELD_WIDTH;
+        marked = 1;
+        for (int col = 0; col < FIELD_WIDTH; col++){
+            if (game.field[field_index+col] == EMPTY){
+                marked = 0;
+                break;
             }
         }
-        //draw
-        
+        if (!marked){
+            if (lines_marked > 0){
+                for (int col = 0; col < FIELD_WIDTH; col++){
+                    unmarked_offset = lines_unmarked * FIELD_WIDTH;
+                    marked_offset = lines_marked * FIELD_WIDTH;
+                    game.field[(piece_bottom - unmarked_offset)+col] = game.field[(piece_bottom - (unmarked_offset + marked_offset)) + col];
+                }
+            }
+            lines_unmarked++;
+        }else{
+            lines_marked++;
+        }
+    }
+    if (lines_marked > 0){
+
+        for (int line = (piece_height + line_start_check - lines_unmarked); line >= lines_marked; line--){
+            for (int col = 0; col < FIELD_WIDTH; col++){
+                game.field[line*FIELD_WIDTH + col] = game.field[(line-lines_marked)*FIELD_WIDTH + col];
+            }
+        }
+        for (int line = 0; line < lines_marked; line++){
+            for (int col = 0; col < FIELD_WIDTH; col++){
+                game.field[line*FIELD_WIDTH + col] = EMPTY;
+            }
+        }
+    }
+    switch (lines_marked){
+        case 0:
+            return 0;
+        case 1:
+            return 2;
+        case 2:
+            return 5;
+        case 3:
+            return 15;
+        case 4:
+            return 60;
+    }
+}
+
+void frame_sync(){
+    static uint64 t0 = 0;
+    static uint64 t1 = 0;
+
+    t1 = (*(uint64*) CLINT_MTIME) / 10000;
+
+    game.dt = t1 - t0;
+    if (game.dt < FRAME_TIME){
+        wait(FRAME_TIME - game.dt);
+        game.dt = FRAME_TIME;
+    }
+    t0 = t1;
+}
+
+void update(){
+    if (game.state != RUNNING) return;
+    uint8 piece_moved;
+    //update ==================================
+    if (game.loop_counter >= UPDATE_LOOP){
+        piece_moved = move_piece_down(&game.current_piece);
+        if (!piece_moved){
+            mark_piece_in_field(game.current_piece);
+            game.score += check_line(game.current_piece.y);
+            game.current_piece = get_next_piece();
+            update_game_over_status();
+            game.has_switched = false;
+        }
+        game.loop_counter = 0xff;
+    }
+    game.shadow_piece = game.current_piece;
+    while(move_piece_down(&game.shadow_piece));
+    game.loop_counter++;
+}
+
+void draw(){
+    //draw =======================================
+    if (game.state == MENU){
         clear();
-        draw_field();
-        draw_piece(p);
-        t2 = (*(uint64*) CLINT_MTIME) / 10000; //15
-        //printf("t2-t1: %h\n", t2-t1);
-        //transfer(FIELD_START,0,FIELD_WIDTH*BLOCK_SIZE + FIELD_START,DEVICE_HEIGHT);
-        transfer(0,0,DEVICE_WIDTH, DEVICE_HEIGHT);
-        i++;
+        uint32 commands_title_size = 5;
+        uint32 commands_size = 3;
+        uint32 diff = (commands_size + 1)*10;
+        uint32 commands_start = 220;
+        draw_string("TETRIS", DEVICE_WIDTH/2, 90, 10, 0xFFEB3E1B, true);
+        draw_string("Commands", 20, 200, commands_title_size, 0xffff9040, false);
+        draw_string("ARROW UP:    rotate", 20, commands_start + diff*1, commands_size, 0xffff0000, false);
+        draw_string("ARROW DOWN:  move down", 20, commands_start+diff*2, commands_size, 0xffff0000, false);
+        draw_string("ARROW LEFT:  move left", 20, commands_start+diff*3, commands_size, 0xffff0000, false);
+        draw_string("ARROW RIGHT: move right", 20, commands_start+diff*4, commands_size, 0xffff0000, false);
+        draw_string("C:           hold", 20, commands_start+diff*5, commands_size, 0xffff0000, false);
+        draw_string("R:           reset", 20, commands_start+diff*6, commands_size, 0xffff0000, false);
+        draw_string("P:           pause", 20, commands_start+diff*7, commands_size, 0xffff0000, false);
+        draw_string("SPACE:       hard drop", 20, commands_start+diff*8, commands_size, 0xffff0000, false);
+        draw_string("PRESSIONE ENTER PARA COMECAR", DEVICE_WIDTH/2, DEVICE_HEIGHT/2 + DEVICE_HEIGHT/4, 4, 0xff00ff00, true);
+    }else{
+        clear();
+        draw_field(game.field);
+        draw_score(game.score);
+        draw_next(game.next_piece, game.next_idx);
+        draw_hold(game.hold_piece, game.has_switched);
+        if (game.state == RUNNING){
+            draw_piece(game.shadow_piece, true);
+            draw_piece(game.current_piece, false);
+        }else{
+            if (game.state == OVER){
+                draw_string("GAME OVER", DEVICE_WIDTH/2, DEVICE_HEIGHT/2, 10, 0xffff0000, true);
+            }else if (game.state == PAUSED){
+                draw_string("PAUSED", DEVICE_WIDTH/2, DEVICE_HEIGHT/2, 8, 0xFFEB3E1B, true);
+            }
+        }
+    }
+    transfer(0,0,DEVICE_WIDTH, DEVICE_HEIGHT);
+}
+
+
+void game_loop(){
+    while (true){
+        frame_sync();
+        treat_keys();
+        update();
+        draw();
     }
 }
 
 void init_game(){
     uint8 num_pages = 1 + (FIELD_HEIGHT*FIELD_WIDTH)/PAGE_SIZE;
-    game_field = (uint8*) alloc(num_pages);
-    init_field();
+
+    game.state = MENU;
+    game.has_switched = false;
+    game.loop_counter = 0;
+    game.current_piece = new_piece();
+    for (int i = 0; i < NEXT_MAX; i++){
+        game.next_piece[i] = new_piece();
+    }
+    game.hold_piece.shape = EMPTY;
+    game.next_idx = 0;
+    game.score = 0;
+    game.field = (uint8*) alloc(num_pages);
+    game.dt = 0;
+    reset_field();
     game_loop();
 }
--- game.h
+++ game.h
@@ -4,30 +4,47 @@
 #define FPS 30
 #define FRAME_TIME 1/FPS
 
-#define FIELD_WIDTH 12
-#define FIELD_HEIGHT 20
-#define FIELD_START ((DEVICE_WIDTH/2) - (FIELD_WIDTH*16))
-#define BLOCK_SIZE 32 //32x32 pixels
 #define TETROMINO_SIZE 4 //4x4 blocks
-#define BLOCKS_PER_TETROMINO 16
-#define NUM_OF_TETROMINOS 7
+#define BLOCK_SIZE 32 //32x32 pixels
+#define BLOCKS_PER_TETROMINO 16 //4x4 blocks = 16 blocks
+#define NUM_OF_TETROMINOS 7 //7 variants of tetrominoes
+#define START_OFFSET 3
 
-uint8 *game_field;
+#define FIELD_WIDTH 12 //12 blocks width
+#define FIELD_HEIGHT (20 + START_OFFSET) //20 blocks height + start offset
+
+#define FIELD_START_X ((DEVICE_WIDTH/2) - (FIELD_WIDTH*(BLOCK_SIZE/2))) //where in width of the device the field starts
+#define FIELD_START_Y (DEVICE_HEIGHT - ((FIELD_HEIGHT + 1)*BLOCK_SIZE)) //where in height of the device the field starts
+#define PIECE_START ((FIELD_WIDTH/2) - (TETROMINO_SIZE/2)) //where in width of the field, the piece starts
+
+#define NEXT_WIDTH 5
+#define NEXT_HEIGHT 12
+
+#define HOLD_WIDTH NEXT_WIDTH
+#define HOLD_HEIGHT 4
+
+//defines how many next pieces we have
+#define NEXT_MAX 3
+
 
 uint8 tetrominoes[NUM_OF_TETROMINOS][BLOCKS_PER_TETROMINO];
-typedef enum pieces{ I, Z, S, O, T, L, J, EMPTY }piece_name;
+typedef enum pieces{ I, Z, S, O, T, L, J, SHADOW, UNAVAILABLE, EMPTY}piece_shape;
 
 typedef struct piece {
     uint32 x, y, rotation;
-    piece_name name;
+    piece_shape shape;
 } piece;
 
 
 
 
-void draw_tetris_block(uint32 x, uint32 y, piece_name index);
-void draw_piece(piece p);
-uint32 rotate(uint32 x, uint32 y, uint32 rotation);
-void draw_field();
-
+void draw_tetris_block(uint32 x, uint32 y, piece_shape index);
+void draw_piece(piece p, uint8 shadow);
+uint32 rotate(uint32 x, uint32 y, uint32 rotation, piece_shape shape);
+void draw_field(uint8 *field);
+void draw_score(uint16 score);
+void draw_shadow(piece p);
+void draw_next(piece *next, uint32 next_idx);
+void draw_hold(piece hold, uint8 has_switched);
+void draw_string(char *string, uint32 x, uint32 y, uint32 size, uint32 color, uint8 center);
 #endif
